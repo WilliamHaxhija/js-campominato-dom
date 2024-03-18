14ea4b5d169c8e2c39c750dcea4b0ccad8e8df73
WilliamHaxhija <williamhaxhija@gmail.com>
2024-03-18 17:12:40 +0100

    feat: add easy mode only

--- js/script.js
+++ js/script.js
@@ -21,20 +21,30 @@ playButton.addEventListener('click', function () {
         numberOfSquares = 49;
         numberOfCellsPerRow = 7
     }
+    //Creo array per le bombe
+    const bombsArray = [];
+    //Genero 16 numeri random nell'array e controllo che non ci siano numeri uguali nell'array (con una funzione){
+    const randomNumber = getArrayRandomUniqueNumber(bombsArray, 16);
+    console.log(bombsArray);
     //Creo le celle
+    let square;
     for (let i = 1; i <= numberOfSquares; i++) {
         const thisNumber = i;
-        const square = generateSquare(i, numberOfCellsPerRow);
+        square = generateSquare(i, numberOfCellsPerRow);
         grid.append(square);
     }
     //Passo in rassegna tutte le celle
     const allSquares = document.querySelectorAll('.ms-cell');
     for (let i = 0; i < allSquares.length; i++) {
         const thisSquare = allSquares[i];
+        console.log(thisSquare.innerHTML);
         //Per ogni cella, creo un evento per interagirci
         thisSquare.addEventListener('click', function () {
+           if (bombsArray.includes((parseInt(thisSquare.innerHTML)))) {
+            this.classList.add('red');
+           } else {
             this.classList.add('lightblue');
-            console.log(this.textContent);
+           }
         });
     }
 });
@@ -48,8 +58,20 @@ playButton.addEventListener('click', function () {
 function generateSquare(number, cellsPerRow) {
     const newSquare = document.createElement('div');
     newSquare.classList.add('ms-cell');
-    newSquare.innerHTML = `<span>${number}</span>`;
+    newSquare.innerHTML = `${number}`;
     newSquare.style.width = `calc(100% / ${cellsPerRow})`;
     newSquare.style.height = `calc(100% / ${cellsPerRow})`;
     return newSquare;
+}
+
+//Funzione che genera numeri random compresi tra 1 e 100 in array pre-esistente
+//array => l'array all'interno del quale pushare i numeri
+//iterations => un numero intero che indica il numero di iterazioni del ciclo while
+function getArrayRandomUniqueNumber(array, iterations) {
+    while (array.length < iterations) {
+        let randomNumber = Math.floor(Math.random() * 100) + 1;
+        if (!array.includes(randomNumber)) {
+            array.push(randomNumber);
+        }
+    }
 }
